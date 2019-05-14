e09f0ce2d1c28cf3d8e59df54455a1a455e0d18a
mckim <bluevoice27@gmail.com>
2019-05-15 00:54:55 +0900

    add: parser exception 추가.

--- exception/custom_exception.py
+++ exception/custom_exception.py
@@ -1,5 +1,7 @@
 #! /usr/bin/python
 # -*- coding: utf-8 -*-
+
+
 class ExitException(Exception):
     __exit_code = 1
 
@@ -11,10 +13,17 @@ class ExitException(Exception):
     def exit(self):
         exit(self.__exit_code)
 
+
 class CannotRunException(ExitException):
     def __init__(self, message, exit_code):
         super().__init__(message, exit_code)
 
+
 class CollectorException(ExitException):
+    def __init__(self, message, exit_code):
+        super().__init__(message, exit_code)
+
+
+class ParserException(ExitException):
     def __init__(self, message, exit_code):
         super().__init__(message, exit_code)
