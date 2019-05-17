99875e5611c8f3bfabcbec26ac5c75d020898764
mckim <bluevoice27@gmail.com>
2019-05-18 05:34:01 +0900

    add: text file storer 구현
    local path 에 text file 로 저장하는 storer 러프하게 구현...

---
+++ store/news_textfile_storer.py
@@ -0,0 +1,66 @@
+#! /usr/bin/python
+# -*- coding: utf-8 -*-
+
+import os
+from pathlib import Path
+from utils.date_util import get_previous_day
+from logzero import logger as log
+
+'''
+new_info dict
+    'origin_create_date': 'yyyyMMddHHmmssSSS'
+    'url': 'xxx',
+    'title': 'xxx',
+    'press_name': 'xxx',
+    'category_name' : 'xxx',
+    'category_en_name': 'xxx',
+    'sub_category_name': 'xxx',
+    'sub_category_en_name': 'xxx'
+    'contents': 'xxxxxxxxxxxxxx'
+'''
+
+
+class DaumNewsTextFileStorer:
+    # text/news/daum/{yyyy-mm-dd}/{main_cate}/{subcate}/{origin_create_date}
+
+    def __init__(self):
+
+        self.__yesterday_date = get_previous_day(1, "%Y%m%d")
+        self.__root_data_path = './data/text/news/daum'
+
+        if not os.path.exists(self.__root_data_path):
+            data_path = Path(self.__root_data_path)
+            data_path.mkdir(parents=True, exist_ok=False)
+
+    # 매번 exist check 는 비효율적.... 추후 변경할 것.
+    def store(self, news_info):
+        store_path = self.__root_data_path + '/' + self.__yesterday_date
+
+        if not os.path.exists(store_path):
+            os.mkdir(store_path)
+
+        # assert category_en_name
+        assert news_info['category_en_name'] is not None \
+            and news_info['category_en_name'] != ''
+
+        store_path += '/' + news_info['category_en_name']
+        if not os.path.exists(store_path):
+            os.mkdir(store_path)
+
+        # assert sub_category_en_name
+        assert news_info['sub_category_en_name'] is not None
+
+        if news_info['sub_category_en_name'] != '-':
+            store_path += '/' + news_info['sub_category_en_name']
+            if not os.path.exists(store_path):
+                os.mkdir(store_path)
+
+        # TODO news contents 빈값인 경우의 처리.
+
+        store_path += '/' + news_info['origin_create_date'] + '.txt'
+
+        if os.path.exists(store_path):
+            log.warn('### file "{0}" exist ... '.format(store_path))
+        else:
+            with open(store_path, mode='wt', encoding='utf-8') as f:
+                f.write(news_info['contents'])
