dc7a8329b9da88d2f148be450401164e7c0ceb82
mckim <bluevoice27@gmail.com>
2019-05-21 04:24:03 +0900

    chage: aws 관련 args 추가.
    aws 관련 args 추가.

--- main.py
+++ main.py
@@ -23,14 +23,29 @@ if __name__ == "__main__" :
         parser.add_argument('--env', type=str, nargs='?', default='dev',
                             help='input run env. \'dev\' or \'stg\' or \'prd\'')
 
+        parser.add_argument('--aws_region', type=str, nargs='?', default='',
+                            help='aws-config region info. not requirement.')
+
+        parser.add_argument('--aws_access_key_id', type=str, nargs='?', default='',
+                            help='aws_access_key_id info. not requirement.')
+
+        parser.add_argument('--aws_secret_access_key', type=str, nargs='?', default='',
+                            help='aws_secret_access_key info. not requirement.')
+
         args = parser.parse_args()
         target_site = args.target.upper()
         role = args.role.lower()
         arg_env = args.env.lower()
 
+        aws_region = args.aws_region
+        aws_access_key_id = args.aws_access_key_id
+        aws_secret_access_key = args.aws_secret_access_key
+
         config_loader = ConfigLoader(arg_env)
 
-        config_loader.load_config()
+        config_loader.load_ionian_config()
+
+        config_loader.set_aws_info(aws_region, aws_access_key_id, aws_secret_access_key)
 
         log.info('### input arg_env : {0}'.format(arg_env))
         log.info('### input role : {0}'.format(role))
