47fb0dc3103feadc9197751a2fb9171576dfbe19
mckim <bluevoice27@gmail.com>
2019-05-26 03:26:22 +0900

    change: dynamo 기능 사용 못하는 경우에 대한 처리 추가.
    dynamo 기능 사용 못할 시에 대한 처리 추가.

--- store/dynamo_storer.py
+++ store/dynamo_storer.py
@@ -4,17 +4,26 @@
 import boto3
 import datetime
 from logzero import logger as log
+from init import constant
+
 
 class DynamoNewsMetaInfoStorer :
 
     __table = None
 
     def __init__(self):
-        dynamodb = boto3.resource('dynamodb')
-        self.__table = dynamodb.Table('news_meta_info')
-
-
+        if constant.CONFIG['aws_enable']:
+            dynamodb = boto3.resource('dynamodb')
+            self.__table = dynamodb.Table('news_meta_info')
+        else:
+            log.warn('### You can not use the dynamo store feature ....')
+            
     def store_to_dynamo(self, news_info_list, site_name):
+
+        if self.__table is None:
+            log.warn('### nothing to do ...')
+            return
+
         # 뉴스 갯수가 많으므로 batch writer 이용.
         with self.__table.batch_writer() as batch:
             for news_info in news_info_list:
