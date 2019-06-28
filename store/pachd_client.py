32a16386e9d83b8d1eb5639669e5b1c8e0935705
Myung Cheol Kim <bluevoice27@gmail.com>
2019-06-28 15:38:58 +0900

    fix: 병렬로 여러개가 실행되는 경우 commit bug 수정.
    병렬로 여러개가 실행되는 경우 commit  관련 꼬임..
    일단 이방법으로 try.

--- store/pachd_client.py
+++ store/pachd_client.py
@@ -1,6 +1,7 @@
 #! /usr/bin/python
 # -*- coding: utf-8 -*-
 
+import traceback
 import python_pachyderm
 from init import constant
 from logzero import logger as log
@@ -12,6 +13,8 @@ class PachdRepoStorer:
     __commit_id = None
     __repo_name = None
     __branch = None
+    __is_complete = False
+    __file_list = []
 
     def __init__(self, repo_name, branch: str = 'master'):
         self.__client = python_pachyderm.PfsClient(
@@ -20,24 +23,41 @@ class PachdRepoStorer:
         self.__repo_name = repo_name
         self.__branch = branch
 
-    def put_file_str(self, file_path, data: str):
-        if self.__commit_id is None:
+    def put_file_str(self, file_path, contents: str):
+        pachd_data = {
+            'file_path': file_path,
+            'contents': contents
+        }
+
+        self.__file_list.append(pachd_data)
+
+    def commit(self, retry_count: int = 0):
+        try:
             self.__commit_id = self.__client.start_commit(self.__repo_name, self.__branch)
 
-        try:
-            log.info('pachd put-file path : {}'.format(file_path))
+            for pachd_data in self.__file_list:
+                self.__client.put_file_bytes(
+                    self.__commit_id, pachd_data['file_path'], pachd_data['contents'].encode('utf-8')
+                )
 
-            self.__client.put_file_bytes(self.__commit_id, file_path, data.encode('utf-8'))
-        except Exception as e:
-            self.__client.delete_commit(self.__commit_id)
-            raise e
-
-    def commit(self):
-        try:
             self.__client.finish_commit(self.__commit_id)
+
             log.info('finish pachd commit. commit_id : {}'.format(self.__commit_id))
+            self.__is_complete = True
+
         except Exception as e:
+            log.error(traceback.format_exc())
+
             self.__client.delete_commit(self.__commit_id)
-            raise e
+
+            if retry_count < 3:
+                self.commit(retry_count + 1)
+            else:
+                is_complete = True
+                raise e
+            
         finally:
-            self.__commit_id = None
+            if self.__is_complete:
+                self.__commit_id = None
+                self.__file_list.clear()
+                self.__is_complete = False
